a2e1c85946c6982dda0b9dd93c5dfebc71505612
Mike Tikh <mike.tikh@gmail.com>
2025-11-13 22:39:52 -0600

    exclude homepage from middleware

--- web/lib/supabase/middleware.ts
+++ web/lib/supabase/middleware.ts
@@ -40,6 +40,7 @@ export async function updateSession(request: NextRequest) {
   // Protected routes
   if (
     !user &&
+    request.nextUrl.pathname !== "/" &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/signup") &&
     !request.nextUrl.pathname.startsWith("/auth")
